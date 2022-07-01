32fb51758d61c8c3d4c9a2f215b918ddbf071290
Israel S. Santos <israel.santos@zukk.com.br>
2022-07-01 18:56:46 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './components/topo/topo.component';
+import { PainelComponent } from './components/painel/painel.component';
+import { ProgressoComponent } from './components/progresso/progresso.component';
+import { TentativasComponent } from './components/tentativas/tentativas.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent,
+    ProgressoComponent,
+    TentativasComponent,
+
   ],
   imports: [
     BrowserModule,
