799e1607419bc62e1009ff30674721ec200ad493
Israel S. Santos <israel.santos@zukk.com.br>
2022-07-02 02:00:24 -0300

    new: added hearts and implemented logic

--- src/app/components/painel/painel.component.ts
+++ src/app/components/painel/painel.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Output } from '@angular/core';
 import { Frase } from "../../shared/frase.model";
 import { FRASES } from "./frases-mock";
 
-
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -12,11 +11,37 @@ export class PainelComponent implements OnInit {
 
   public instrucao: string = "Traduza a seguinte frase:";
   public frases: Frase[] = FRASES;
+  public resposta: string = "";
+  public rodada: number = 0;
+  public fraseRodada: Frase = this.frases[0];
+  public progresso: number;
+  public campoResposta: string = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
-    console.log(this.frases)
+  constructor() {
+    this.progresso = 0;
+    this.updateAnswer();
   }
 
+  ngOnInit(): void {
+  }
+
+  getValue(e: Event): void {
+    this.resposta = (<HTMLInputElement>e.target).value;
+  }
+
+  checkAnswer(): void {
+    if (this.resposta.trim().toLowerCase() === this.fraseRodada.frasePtBr.toLowerCase()) {
+      this.rodada = this.rodada + 1;
+      this.updateAnswer();
+      this.progresso = this.progresso + (100 / this.frases.length);
+    }
+    else {
+
+    }
+  }
+  updateAnswer(): void {
+    this.fraseRodada = this.frases[this.rodada];
+    this.resposta = "";
+  }
 }
+
--- src/app/components/tentativas/tentativas.component.ts
+++ src/app/components/tentativas/tentativas.component.ts
@@ -11,11 +11,36 @@ export class TentativasComponent implements OnInit {
   fillOne: string = "red";
   fillTwo: string = "red";
   fillThree: string = "red";
+  public tentativasRestantes: number = 3;
 
   constructor() {
+    this.handleTentativas();
   }
 
   ngOnInit(): void {
+  }
+
+
+  handleTentativas(): void {
+    if (this.tentativasRestantes === 3) {
+      this.fillOne = "red";
+      this.fillTwo = "red";
+      this.fillThree = "red";
+    } else if (this.tentativasRestantes === 2) {
+      this.fillOne = "black";
+      this.fillTwo = "red";
+      this.fillThree = "red";
+
+    } else if (this.tentativasRestantes === 1) {
+      this.fillOne = "black";
+      this.fillTwo = "black";
+      this.fillThree = "red";
+    }
+    else if (this.tentativasRestantes === 0) {
+      this.fillOne = "black";
+      this.fillTwo = "black";
+      this.fillThree = "black";
+    }
   }
 
 }
